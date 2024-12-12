78bdc0f1bfd8eddc29e23a415c0e001b6f385923
Rémi Vanicat <vanicat@debian.org>
2024-12-12 11:57:56 +0100

    detect loop when crossing
    Not enough

--- day6.py
+++ day6.py
@@ -15,7 +15,7 @@ exem = """....#.....
 class Labyrinthe:
     labi:list[list[bool]]
     
-    seen:list[list[bool]]
+    seen:list[list[set[tuple[int, int]]]]
     
     pos_i:int
     pos_j:int
@@ -30,6 +30,13 @@ class Labyrinthe:
         ">": (0, 1),
         "<": (0, -1),
         "v": (1, 0),
+    }
+
+    DIR_TO_SYMB = {
+        (-1, 0): "^",
+        (0,  1): ">",
+        (0, -1): "<",
+        (1,  0): "v",
     }
 
     TURN = {
@@ -48,7 +55,7 @@ class Labyrinthe:
             [lines[i][j] == "#" for j in range(self.width)] for i in range(self.height)
         ]
 
-        self.seen = [[False for j in range(self.width)] for i in range(self.height)] 
+        self.seen = [[set() for j in range(self.width)] for i in range(self.height)] 
 
         for i in range(self.height):
             for j in range(self.width):
@@ -56,7 +63,7 @@ class Labyrinthe:
                     self.dir_i, self.dir_j = self.SYMB_TO_DIR[lines[i][j]]
                     self.pos_i = i
                     self.pos_j = j
-                    self.seen[i][j] = True
+                    self.seen[i][j].add((self.dir_i, self.dir_j))
 
     def __repr__(self) -> str:
         return f"Labyrinthe({self.labi}, {self.pos_i}, {self.pos_j})"
@@ -66,13 +73,20 @@ class Labyrinthe:
              (False, False) : ".",
              (True, False) : "#",
              (False, True): "*"
-         }
-        return "\n".join(
-            "".join( to_char[self.labi[i][j], self.seen[i][j]]
-                    for j in range(self.width)
-            )
-            for i in range(self.height)
-        )
+        }
+        def yield_chars(i):
+            for j in range(self.width):
+                if self.labi[i][j]:
+                    yield "#"
+                elif not self.seen[i][j]:
+                    yield "."
+                elif len(self.seen[i][j]) != 1:
+                    yield "*"
+                else:
+                    elem:set = self.seen[i][j]
+                    yield self.DIR_TO_SYMB[next(iter(self.seen[i][j]))]
+
+        return "\n".join("".join(yield_chars(i)) for i in range(self.height))
     
     def nb_seen(self) -> int:
         c = 0
@@ -88,10 +102,11 @@ class Labyrinthe:
         nj = self.pos_j + self.dir_j
         if ni < 0 or ni >= self.height:
             return False
-        if nj < 0 or nj >= self.width:
+        elif nj < 0 or nj >= self.width:
             return False
-        if self.labi[ni][nj]:
+        elif self.labi[ni][nj]:
             self.dir_i, self.dir_j = self.TURN[(self.dir_i, self.dir_j)]
+            # return self.step()
         else:
             self.pos_i = ni
             self.pos_j = nj
@@ -99,9 +114,19 @@ class Labyrinthe:
 
     def parcours(self) -> int:
         while self.step():
-            self.seen[self.pos_i][self.pos_j] = True
-        
+            self.seen[self.pos_i][self.pos_j].add((self.dir_i, self.dir_j))
         return self.nb_seen()
+
+        
+    def parcours2(self) -> int:
+        while self.step():
+            if self.TURN[(self.dir_i, self.dir_j)] in self.seen[self.pos_i][self.pos_j]:
+                print(f"cycle found at ({self.pos_i}, {self.pos_j})")
+            self.seen[self.pos_i][self.pos_j].add((self.dir_i, self.dir_j))
+            print(self)
+
+        return self.nb_seen()
+
 
 lb_exem = Labyrinthe(StringIO(exem))
 print(lb_exem)
@@ -109,6 +134,10 @@ lb_exem.step()
 print(lb_exem)
 lb_exem = Labyrinthe(StringIO(exem))
 print(lb_exem.parcours())
+print(lb_exem)
+
+lb_exem = Labyrinthe(StringIO(exem))
+print(lb_exem.parcours2())
 print(lb_exem)
 
 import urllib.request
@@ -119,4 +148,5 @@ PB_FILE_NAME = "input-06-1.txt"
 with open(PB_FILE_NAME) as f:
     lb = Labyrinthe(f)
     print(lb.parcours())
+
 
